4438664f4175015c469c108c04d24bb979ae91ac
kanzakirei <kanzakirei.aqua@gmail.com>
2023-09-15 23:41:01 +0900

    jenkins build

--- bat/createPostHTML.bat
+++ bat/createPostHTML.bat
@@ -52,7 +52,6 @@ for /f "delims=" %%t in (%1) do (
     set row=    ^<p^>%%t^<br^>^</p^>
     setlocal enabledelayedexpansion
     set row=!row:../=../../!
-    echo !row!
     echo !row!>>main.tmp
     endlocal
 )
