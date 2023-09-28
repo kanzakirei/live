82143fe95300933e85d75e00e21f539a762d4f18
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2023-09-28 09:08:46 +0900

    deletePostHTML.bat を更新

--- bat/deletePostHTML.bat
+++ bat/deletePostHTML.bat
@@ -6,4 +6,15 @@ for %%f in (*.html) do (
   del %%f
 )
 popd > nul
+pushd "../posts" > nul
+for %%f in (*.html) do (
+  call :DeleteHTML ../html/posts/%%f
+)
+popd > nul
+exit /b 0
+
+:DeleteHTML
+if not exist %~dpn1.txt (
+  del %~nx1
+)
 exit /b 0
