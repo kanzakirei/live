cfb6eef209c729cce7a4e6be44b31867efb6fedb
kanzakirei <kanzakirei.aqua@gmail.com>
2023-10-04 21:04:29 +0900

    jenkins build

--- bat/createPostHTML.bat
+++ bat/createPostHTML.bat
@@ -17,11 +17,11 @@ exit /b 0
 
 :MainFile
 if exist %1 (
-    if exist %2 (
-        if "%~t2" GTR "%~t1" (
-            rem exit /b 0
-        )
+  if exist %2 (
+    if "%~t2" GTR "%~t1" (
+      exit /b 0
     )
+  )
 )
 
 set InDir=%1
@@ -31,9 +31,9 @@ call :PostFile %InDir%
 
 type nul>%OutDir%
 for /f "delims=" %%t in (%BaseHTML%) do (
-    (echo.%%t) >> %OutDir%
-    echo "%%t" | find "<main>" > nul
-    if not ERRORLEVEL 1 type main.tmp>>%OutDir%
+  (echo.%%t) >> %OutDir%
+  echo "%%t" | find "<main>" > nul
+  if not ERRORLEVEL 1 type main.tmp>>%OutDir%
 )
 echo CreatePost %~nx2
 exit /b 0
@@ -43,7 +43,7 @@ type nul>main.tmp
 (echo.      ^<div class="content"^>) >> main.tmp
 (echo.        ^<br^>^<br^>) >> main.tmp
 for /f "tokens=1* delims=: eol=" %%x in ('findstr /n "^" %1') do (
-    (echo.        ^<p^>%%y^<br^>^</p^>) >> main.tmp
+  (echo.        ^<p^>%%y^<br^>^</p^>) >> main.tmp
 )
 (echo.        ^<br^>^<br^>) >> main.tmp
 (echo.      ^</div^>) >> main.tmp
