b16bb16c65ed82cb5677368f8409a6200a4a4e9b
kanzakirei <kanzakirei.aqua@gmail.com>
2023-10-04 21:33:16 +0900

    jenkins build

--- bat/convertAVIF.bat
+++ bat/convertAVIF.bat
@@ -4,9 +4,7 @@ chcp 65001 > nul
 pushd "../img" > nul
 for /r %%f in (*.png,*.jpg) do (
   if not exist %%~dpnf.avif (
-	pushd "../bat" > nul
-    call cavif.exe ../img/%%f
-	popd > nul
+    call "../bat/cavif.exe" %%f
   )
 )
 popd > nul
