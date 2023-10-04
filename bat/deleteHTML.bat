58fd1eb44768ffbebdb892fdd01436ce7f013005
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2023-10-04 09:43:39 +0900

    Update deleteHTML.bat

--- bat/deleteHTML.bat
+++ bat/deleteHTML.bat
@@ -1,20 +1,12 @@
 @echo off
 chcp 65001 > nul
 
-pushd "../html/posts" > nul
+popd > nul
+pushd "../" > nul
 for %%f in (*.html) do (
-  del %%f
+  if not exist ../html/%%f (
+    del %%f
+  )
 )
 popd > nul
-pushd "../posts" > nul
-for %%f in (*.html) do (
-  call :DeleteHTML ../html/posts/%%f
-)
-popd > nul
-exit /b 0
-
-:DeleteHTML
-if not exist %~pn1.txt (
-  rem del %~nx1
-)
 exit /b 0
