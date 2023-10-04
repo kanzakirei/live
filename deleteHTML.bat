5fcfd8ffd513228dd287dd3472bdb98c4863b76b
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2023-10-04 09:34:47 +0900

    Create deleteHTML.bat

---
+++ deleteHTML.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 > nul
+
+pushd "../html/posts" > nul
+for %%f in (*.html) do (
+  del %%f
+)
+popd > nul
+pushd "../posts" > nul
+for %%f in (*.html) do (
+  call :DeleteHTML ../html/posts/%%f
+)
+popd > nul
+exit /b 0
+
+:DeleteHTML
+if not exist %~pn1.txt (
+  rem del %~nx1
+)
+exit /b 0
