f8890b349ad12a91de4a483093a24a536f864cf0
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2023-10-05 09:54:34 +0900

    build.bat を更新

--- bat/build.bat
+++ bat/build.bat
@@ -3,6 +3,10 @@ chcp 65001 > nul
 cd %~dp0
 
 call InsertText.bat a.txt b.txt ^<key^>
+if %errorlevel% neq 0 (
+  echo Failure: InsertText.bat
+  exit /b %errorlevel%
+)
 
 call createPostHTML.bat
 if %errorlevel% neq 0 (
