aad20fd85769e1bd1f0ad643b15914e0e8e0f139
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2023-10-05 09:39:50 +0900

    Add files via upload

---
+++ bat/InsertText.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+set InportFile=%1
+set ExportFile=%1
+set InsertFile=%2
+set KeyString=%3
+
+if not exist %InsertFile% (
+  echo Not exist %InsertFile%
+  exit /b 1
+)
+if not exist %InportFile% (
+  echo Not exist %InportFile%
+  exit /b 1
+)
+if not exist %ExportFile% (
+  echo Not exist %ExportFile%
+  exit /b 1
+)
+
+type nul>temp.txt
+for /f "tokens=1* delims=: eol=" %%x in ('findstr /n "^" %InportFile%') do (
+  (echo.%%y) >> temp.txt
+  (echo %%y) | find %KeyString% > nul
+  if not ERRORLEVEL 1 type %InsertFile%>>%ExportFile%
+)
+exit /b 0
