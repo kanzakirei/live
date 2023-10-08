432fd7222ba4e72cafd0a1c8cd83f1033de3e8df
kanzakirei <kanzakirei.aqua@gmail.com>
2023-10-08 22:04:42 +0900

    jenkins build

--- bat/InsertText.bat
+++ bat/InsertText.bat
@@ -1,25 +1,35 @@
+@echo off
+chcp 65001 > nul
 set InportFile=%1
 set InsertFile=%2
 set KeyString=%3
 set ExportFile=%4
-set TempFile=temp.txt
+set Result=0
+set TempFile=temp
 
-if not exist %InportFile% (
-  echo Not exist %InportFile%
+if not exist "%InportFile%" (
+  echo Inport file ^(%%1^) not exist.
   exit /b 1
 )
-if not exist %InsertFile% (
-  echo Not exist %InsertFile%
+if not exist "%InsertFile%" (
+  echo Insert file ^(%%2^) not exist.
   exit /b 1
 )
-if not exist %ExportFile% (
+if %KeyString%=="" set Result=1
+if %Result%==1 (
+  echo Search key ^(%%3^) is empty.
+  exit /b 1
+)
+if "%ExportFile%"=="" (
+  echo Export file ^(%%4^) not exist.
+  echo Insert into import file.
   set ExportFile=%InportFile%
 )
 
 type nul>%TempFile%
 for /f "tokens=1* delims=: eol=" %%x in ('findstr /n "^" %InportFile%') do (
   (echo.%%y) >> %TempFile%
-  (echo "%%y") | find "%KeyString%" > nul
+  (echo "%%y") | find %KeyString% > nul
   if not ERRORLEVEL 1 (
     type %InsertFile%>>%TempFile%
     (echo.) >> %TempFile%
