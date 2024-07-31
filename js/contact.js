f3e6757a57ea9b4f425cd1a089c8a248a576b3cc
kanzakirei <kanzakirei.aqua@gmail.com>
2024-08-01 00:01:19 +0900

    jenkins deploy

--- js/contact.js
+++ js/contact.js
@@ -6,9 +6,8 @@ function onClickedContactButton() {
 
   if (nameForm && nameForm.value && messageForm && messageForm.value) {
     var api = "https://script.google.com/macros/s/AKfycbwyoAxPpNTmtEjGvoYi5qdz4cu8SvUfJpWAmhe-wSGQHCWK-kFSmn2UddnrUdbdo4yO/exec";
-    var json = JSON.stringify({ name: nameForm.value, message: messageForm.value });
-
-    requestPost(api, json, function (data) {
+    var parameter = "?name=" + nameForm.value + "&message=" + messageForm.value;
+    requestGet(api + parameter, function (data) {
       document.getElementById('formWrapper').style.display = 'none';
       document.getElementById('thxMessage').style.display = 'block';
     });
